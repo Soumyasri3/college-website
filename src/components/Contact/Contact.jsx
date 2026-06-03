4ec9c90cfce2551dd82879058654f984da4e1b6a
Soumyasri3 <soumyashreethatoi36933@gmail.com>
2026-06-03 09:57:06 +0530

    Add Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h2>Contact Us</h2>
+      <p>Email: info@giet.edu</p>
+      <p>Phone: +91 9876543210</p>
+    </div>
+  );
+};
+
+export default Contact;
